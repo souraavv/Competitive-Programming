5dbbfd12b8a4afb31d58ce5318156fe5b0dcc0a4
Sourav Sharma <46745355+vsourav@users.noreply.github.com>
2020-02-08 08:31:37 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,45 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define int long long
+
+#define int long long int
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(v) v.begin(), v.end()
+#define f(i, l, n) for (int i = l; i < n; ++i)
 #define pb push_back
-int n;
-const int N = 2e5+5;
-vector<int> g[N];
-int parent[N];
 
 
-void make_set(){
-    for(int i=1;i<=n;++i)parent[i]=i;
-}
+vi p;
+vi sz;
 
 int find_set(int x){
-    if(x==parent[x])return x;
-    return parent[x]= find_set(parent[x]); //optimization
+    return x==p[x]?x:x=find_set(p[x]);
 }
 
 void union_set(int x,int y){
     x = find_set(x);
     y = find_set(y);
-    // make either parent of y to x or other way round : we can use size optimization
-    parent[y]=x;
-}
-
-void dfs(int u){
-    cout<<u<<" ";
-    for(int v: g[u])dfs(v);
-}
-
-int32_t main(){
-    ios::sync_with_stdio(false);cin.tie(NULL);
-    int x,y;
-    cin>>n;
-   
-    make_set();
-
-    for(int i=0;i<n-1;++i){
-        cin>>x>>y;
-        union_set(x,y);
-        g[x].push_back(y);
+    if(x!=y){
+        p[y]=x;
+        sz[x]+=sz[y];
+        sz[y]=0;
     }
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin>>n;
+    p=vi(n+1);
+    sz=vi(n+1,1);
     
+    for(int i=1;i<=n;++i)p[i]=i;
+    
+    
+    return 0;
 }
