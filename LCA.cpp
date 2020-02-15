6a077d44314be22950ec6d2c9c3a3d38dbea12e1
Sourav Sharma <46745355+vsourav@users.noreply.github.com>
2020-02-15 14:45:57 +0530

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -1,71 +1,99 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define LG 11
-#define N 1024
-int P[N][20];
-int lvl[N];
-vector<int>adj[N];
+
+#define int long long int
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define f(i, l, n) for (int i = l; i < n; ++i)
+#define pb push_back
+
+
+const int N=1e5;
+int Par[N][20];
+vi lvl;
+vvi g;
 
 void dfs(int u,int par){
-    P[u][0]=par;
+    Par[u][0]=par;
     lvl[u]=1+lvl[par];
-    for(int v:adj[u]){
-        if(v==par)continue;
-        dfs(v,u);
+    for(int v: g[u]){
+        if(v!=par){
+            dfs(v,u);
+        }
     }
 }
 
 int lca(int u,int v){
     if(lvl[u]<=lvl[v])swap(u,v);
-    int lg;
-    for(lg=0;lvl[u]>=(1<<lg);++lg);
-    lg--;
     
-    for(int i=lg;i>=0;--i){
-        if(lvl[u]-(1<<i)>=lvl[v])
-            u=P[u][i];
+    int jump;
+    for(jump=0;(lvl[u]>=(1<<jump));++jump);
+    jump--;
+    
+    for(int i=jump;i>=0;--i){
+        if(lvl[v]<=lvl[u]-(1<<i))u=Par[u][i];
     }
     
-    if(u==v)
-      return u;
-    
-    for(int i=lg;i>=0;--i){
-        if( P[u][i]!=-1 and P[u][i]!=P[v][i]){
-            u=P[u][i],v=P[v][i];
+    if(u==v)return u;
+
+    for(int i=jump;i>=0;--i){
+        if(Par[u][i]!=-1 and Par[u][i]!=Par[v][i]){
+            u=Par[u][i],v=Par[v][i];
         }
     }
     
-    return P[u][0];
+    return Par[u][0];
+    
 }
 
 
-int main(){
+int dist(int a,int b){
+    return(lvl[a]+lvl[b]-2*lvl[lca(a,b)]);
+}
+
+int32_t main(){
+    
     int n;
     cin>>n;
-
-    for(int i=1;i<n;++i){
+    g=vvi(n+1);
+    lvl=vi(n+1,0);
+    for(int i=0;i+1<n;++i){
         int u,v;
         cin>>u>>v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        g[u].pb(v);
+        g[v].pb(u);
     }
-    for(int j=0;j<20;++j)for(int i=0;i<=n;++i)P[i][j]=-1;
+    
+    for(int i=1;i<=n;++i)for(int j=0;j<20;++j)Par[i][j]=-1;
+    
     lvl[0]=-1;
     dfs(1,0);
     
-    for(int i=1; i<LG; i++){
-        for(int j=1;j<=n;++j)
-            if (P[j][i-1] != -1)
-                P[j][i] = P[P[j][i-1]][i-1];
-	}
-
-    
-    
-    cout<<lca(10,3);
-    
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<20;++j){
+            if(Par[i][j-1]!=-1)Par[i][j]=Par[Par[i][j-1]][j-1];
+        }
+    }
+    cout<<lca(8,4)<<"\n";
+    cout<<dist(8,4);
+    return 0;
 }
 
 /*
+
+Sample Tree: 
+8
+1 2
+1 3
+1 4
+2 5
+2 6
+6 7
+7 8
+
+Sample Problem: 
 
 Problem list: https://www.codechef.com/LTIME77A/problems/DDQUERY
 
