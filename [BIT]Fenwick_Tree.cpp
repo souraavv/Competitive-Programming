0abc4df8509f3785c798c6015bb71ff1381f7227
Sourav Sharma <46745355+vsourav@users.noreply.github.com>
2020-02-26 11:25:38 +0530

    Update [BIT]Fenwick_Tree.cpp
    x - ( x & -x ) = Remove the last set bit
    x + ( x & - x) = Add last set bit to x 
                                                                      (13-13) +  (9-12)  +  (1-8)
    To get a query of say prefix sum till 13 =   1101 => 1100  => 1000

--- [BIT]Fenwick_Tree.cpp
+++ [BIT]Fenwick_Tree.cpp
@@ -1,68 +1,41 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 
-class BIT{
-    private: 
-        vector<int> m_array;
-    
-    public: 
-        BIT(vector<int>a){
-            m_array=vector<int>(a.size()+1,0);
-            for(int i=0;i<a.size();++i){
-                m_array[i+1]=a[i];
-            }
-            
-            for(int i=1;i<m_array.size();++i){
-                int idx=i+(i&(-i)); // adding 1 to last set bit infact each one is contributing either to its upward parent or next parent;
-                if(idx<m_array.size()){
-                    m_array[idx]+=m_array[i];
-                } 
-            }
-        }
-        
-        int prefix_query(int idx)const{
-            int result=0;
-            ++idx;
-            for(;idx>0;idx-=idx& -idx){
-                result+=m_array[idx];
-            }
-            return result;
-        }
-        
-        int range_query(int from_idx,int to_idx){
-            
-            if(from_idx==0){
-                return prefix_query(to_idx);
-            }
-            
-            else return prefix_query(to_idx)-prefix_query(from_idx-1);
-        }
-        
-        void update(int idx,int val){
-            for(++idx;idx<m_array.size();idx+=idx & -idx){
-                m_array[idx]+=val;
-            }
-        }
-        
-        
-};
+const int N=1e5;
+int bit[N];
+int n;
 
 
+void add(int idx,int val){
+    while(idx<=n){
+        bit[idx]+=val;
+        idx+=idx & -idx;
+    }
+}
+
+int query(int idx){
+    int sum=0;
+    while(idx){
+        sum+=bit[idx];
+        idx-=idx & -idx;
+    }
+    return sum;
+}
+
+int range_query(int idx1,int idx2){
+	return (query(idx1)-query(idx2-1));	
+}
 
 int main(){
     
-    std::vector<int> a{1,1,0,1,0,1,1,1,2,0,0,1,0,1 };
+    cin>>n;
+    for(int i=1;i<=n;++i){
+        int a;
+        cin>>a;
+        add(i,a);
+    }
     
-    BIT bit(a);
+    cout<<range_query(14,5);
     
-    std::cout << "Prefix sum of first 13 elements: \t" << bit.prefix_query(12) << std::endl;
-	std::cout << "Prefix sum of first 7 elements: \t" << bit.prefix_query(6) << std::endl;
-	std::cout << "Range sum from index 4 to index 5: \t" << bit.range_query(4, 5) << std::endl;
-    bit.update(4, 2);
-    
-    std::cout << "Prefix sum of first 13 elements: \t" << bit.prefix_query(12) << std::endl;
-	std::cout << "Prefix sum of first 7 elements: \t" << bit.prefix_query(6) << std::endl;
-	std::cout << "Range sum from index 4 to index 4	: \t" << bit.range_query(4, 4) << std::endl;
-	
-	return 0;
 }
