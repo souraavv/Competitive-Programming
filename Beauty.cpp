95073fab6c32ec4fec29dd294618e847aae2224d
Sourav Sharma <46745355+vsourav@users.noreply.github.com>
2020-03-18 13:22:36 +0530

    Update Beauty.cpp

--- Beauty.cpp
+++ Beauty.cpp
@@ -28,5 +28,47 @@
     
     Very good problem: https://codeforces.com/problemset/problem/1325/D
 
-
+3) Array a of size n ( n<=10^5 ) and q (q<=10^5) query where each query ask to xor the p with each element of array A and count the
+    total number of set bit odd and even. 
+    
+    Some Observation : If a number k is xor with say p =[0,1], then what will happen
+                        
+                       Case 1: p=0
+                            If total no. of set bit in k is odd and p=0 then after xor (k^p = k^0 = k) 
+                            doesn't change and If k is even then also. Infact as k^0 = k so parity of set
+                            bit doesn't change.
+                       
+                       Case 2: p=1
+                            If k has odd no. of set bit then k^1 will have even no. of set bit.
+                            If k has even no. of set bit then k^1 will have odd no. of set bit. 
+                            
+                            e.g 1110000 ^ 1 = 1110001 (3 -> 4)    
+                                1100001 ^ 1 = 1100000 (3 -> 2)
+                                
+                                and 
+                                
+                                111100 ^ 1 = 111101 (4 -> 5)
+                                111001 ^ 1 = 111000 (4 -> 3)
+                              
+                             So this mean each set bit in p [ the no. which we are xoring with each element]
+                             will alter the no. of set bit . If it is even then it will become odd and vice vers
+                             
+                             So What we have to do precount the no. of __builtin_popcount(x) which are odd
+                             and even. And then count this for p .
+                             If p has even parity of set bit the p is equivalent to = 1
+                             if p has even parity of set bit the p is equivalent to = 0
+                             
+                             Why ?
+                             
+                             A no. get the same parity after 2 turn [ even-> | odd -> even | ]
+                             So if p has odd no. of set bit ( 2x + 1) then at final 
+                                                                                    even -> ...... -> odd
+                                                                                and odd -> ...... -> even
+                             else if p has even no. of set bit 
+                                                            even -> ..... -> even
+                                                            odd -> .... -> odd
+                             
+    
+    
+    
 */
