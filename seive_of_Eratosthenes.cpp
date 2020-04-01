e83034aaa6c39bfefd64dc455a4189d506b865ac
Sourav Sharma <souravsharma2468@gmail.com>
2020-04-01 11:21:28 +0530

    Update seive_of_Eratosthenes.cpp

--- seive_of_Eratosthenes.cpp
+++ seive_of_Eratosthenes.cpp
@@ -8,13 +8,16 @@ int main(){
     vector<bool>is_prime(n+1,true);
     is_prime[0]=is_prime[1]=false;
     
-    for(int i=2;i<=n*n;++i){
-        if(is_prime[i] and i*i<=n ){
+    for(int i=4;i<=n;i+=2)
+            is_prime[i]=false;
+    
+    for(int i=3;i*i<=n;++i){
+        if(is_prime[i]){
             for(int j=i*i;j<=n;j+=i)
                 is_prime[j]=false;
         }
     }
-    // Asymptotic complexity-- O(nlog(log(n)))
+    // Asymptotic complexity-- O(nlog(log(n))) = N/2 + N/3 + N/5 + N/7 + .... 
     // though because we are going till root n-- complexity will be O(n)+O(n ln ln (sqrt(n)))
     
     for(int i=1;i<=n;++i)if(is_prime[i])cout<<i<<", ";
