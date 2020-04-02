168e939a8c0f956e3d6ac2db1b13ed6bed1cc723
Sourav Sharma <souravsharma2468@gmail.com>
2020-04-02 06:59:25 +0530

    Update Binary Exponentiation.cpp

--- Binary Exponentiation.cpp
+++ Binary Exponentiation.cpp
@@ -11,23 +11,43 @@ int pow(int a,int b){
         if(b%2)
             ans*=a;
         a*=a;
-        b/=2;
+        b>>=1;
     }
     return ans;
 }
 
+int add(int a,int b){
+    a+=b;
+    if(a>=mod)
+        return a-mod;
+    return a;
+}
+
+int sub(int a,int b){
+    a-=b;
+    if(a<0)
+        return a+mod;
+    return a;
+}
+
+int multiply(int a,int b){
+    int k=(a*b)%m;
+    return k;   
+}
 // since a.b mod m =[ (a mod m)*(b mod m) ]mod m ; since both a and be are same 
 int power(int a,int b,int m){
     int ans=1;
     a%=m;
     while(b>0){
         if(b%2)
-            ans=ans*a%m;
-        a=a*a%m;
+            ans=(ans*a)%m;
+        a=(a*a)%m;
         b/=2;
     }
     return ans;
 }
+
+
 int32_t main(){
     int k = pow(2,20);
     int p = power(2,20,mod);
