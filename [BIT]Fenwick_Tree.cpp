8862de14097eb61b0364d4a659b46a626b104e20
Sourav Sharma <souravsharma2468@gmail.com>
2020-05-18 13:03:32 +0530

    Update [BIT]Fenwick_Tree.cpp

--- [BIT]Fenwick_Tree.cpp
+++ [BIT]Fenwick_Tree.cpp
@@ -1,38 +1,37 @@
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int N=1e5;
+const int N = 1e6 + 10;
 int bit[N];
 int n;
 
-
-void add(int idx,int val){
-    for(++idx;idx<=n;idx+=idx & -idx)
-	    bit[idx]+=val;
+void add(int idx, int val) {
+  for (++idx; idx <= n; idx += idx & -idx)
+    bit[idx] += val;
 }
 
-int query(int idx){
-    int sum=0;
-    for(++idx;idx>0;idx-=idx & -idx)
-	    sum+=bit[idx];
-    return sum;
+int query(int idx) {
+  int sum = 0;
+  for (++idx; idx > 0; idx -= idx & -idx)
+    sum += bit[idx];
+  return sum;
 }
 
-int range_query(int i,int j){
-	if(i==0)return query(j);
-	else return query(j)-query(i-1);
+int range_query(int i, int j) {
+  if (i == 0)
+    return query(j);
+  else
+    return query(j) - query(i - 1);
 }
 
-int main(){
-    
-    cin>>n;
-    for(int i=1;i<=n;++i){
-        int a;
-        cin>>a;
-        add(i,a);
-    }
-    
-    cout<<range_query(14,5);
-    
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; ++i) {
+    int a;
+    cin >> a;
+    add(i, a);
+  }
+
+  cout << range_query(14, 5);
 }
