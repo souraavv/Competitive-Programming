9247c6dd3d7aa39d166f65e2ffa478ca4df29e62
Sourav Sharma <souravsharma2468@gmail.com>
2020-06-22 10:29:12 +0530

    Update modulo.cpp

--- modulo.cpp
+++ modulo.cpp
@@ -1,6 +1,3 @@
-// computing all i^-1 up to n^-1 in O(n)
-// Using the fact n^-1 = (n - 1)! / (n)! = (fact[n - 1] * ifact[n]) % mod;
-
 
 // computing factorial upto n in O(n).
 
@@ -22,6 +19,10 @@ ifact[n] = mpow(fact[n], m - 2);
 for (int i = n - 1; i >= 0; i--) {
   ifact[i] = ((i + 1) * ifact[i + 1]) % m;
 }
+
+// computing all i^-1 up to n^-1 in O(n)
+// Using the fact n^-1 = (n - 1)! / (n)! = (fact[n - 1] * ifact[n]) % mod;
+
 
 long long inverse[n + 1];
 for (int i = 1; i <= n; i++) {
