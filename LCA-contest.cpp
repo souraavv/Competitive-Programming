201cc7bdd53cd2a6a10b3bcfdc0ac16f17a3dff9
Sourav Sharma <souravsharma2468@gmail.com>
2020-07-19 15:20:08 +0530

    Update LCA-contest.cpp

--- LCA-contest.cpp
+++ LCA-contest.cpp
@@ -1,18 +1,31 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
-#define print(x) cerr << #x << " is " << x << endl;
+#define hey(x) cerr << #x << " : " << x << "\n";
 #define int long long int
 #define ll long long
 #define vi vector<int>
-#define vvi vector<vector<int>>
-#define vpi vector<pair<int, int>>
-#define all(v) (v).begin(), (v).end()
+#define vvi vector<vector<int> >
+#define vpi vector<pair<int, int> >
+#define vvpi vector<vector<pair<int, int> > >
+#define all(v) (v).begin(), (v).end()	
+#define rall(v) (v).rbegin(), (v).rend()
 #define pii pair<int, int>
 #define pb push_back
+#define SZ(x) (int)(x).size()
+#define inf 1e12
+#define F first
+#define S second
+#define fill(v, val) memset((v), val, sizeof(v))
+#define PI 3.1415926535897932384626
+#define out cout << fixed << setprecision(12)
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define hey2(x, y) cerr << #x << " : " << x << "," << #y << " : " << y << '\n';
 
-const int N = 5e5 + 10;
+const int N = 1e6 + 10;
 const int mod = 1e9 + 7;
+const double pi = acos(-1);
 
 vi g[N];
 int u, v;
@@ -28,11 +41,8 @@ void dfs(int u, int par){
         if(dp[u][i-1] != -1)
             dp[u][i] = dp[dp[u][i-1]][i-1];
     
-    for(int v: g[u]){
-        if(v != par){
+    for(int v: g[u]) if(v != par)
             dfs(v, u);
-        }
-    }
 }
 
 
@@ -61,26 +71,19 @@ int32_t main()
 {    
     ios::sync_with_stdio(false);
     cin.tie(0);
-
-    
+    // ----------------------- Initialization ------------------------
     int n;
     cin >> n;
-    
     for(int i = 0; i + 1 < n; ++i){
         cin >> u >> v;
         g[u].pb(v);
         g[v].pb(u);
     }
-   
     memset(dp, -1, sizeof(dp));
     lvl[0] = -1;
+    
     dfs(1, 0);
+    // ---------------------write code below-----------------------------
     
-    /* ---------------write code below---------------------------- */
-    
- 
- 
-   
-   
     return 0;
 }
