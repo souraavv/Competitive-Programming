4137e0ddf5c6485c2f7481d59b7d75dd6c3b4651
Sourav Sharma <souravsharma2468@gmail.com>
2020-08-05 16:26:47 +0530

    Update seive_of_Eratosthenes.cpp

--- seive_of_Eratosthenes.cpp
+++ seive_of_Eratosthenes.cpp
@@ -1,18 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 const int N=1e6+10;
 
-vector<bool> prime(N+1,0);
-
+vector<bool> prime(N + 1, 1);
 void seive(){
-
-    prime[0]=prime[1]=0;
-    
-    for(int i=2;i*i<=N;++i){
-        if(prime[i]){
-            for(int j=i*i;j<=N;j+=i)
-                prime[j]=0;
+    prime[0] = prime[1] = 0;
+    for(int i = 2;i * i <= N; ++i) {
+        if(prime[i]) {
+            for(int j = i * i; j <= N;j += i)
+                prime[j] = 0;
         }
     } 
 }
+
