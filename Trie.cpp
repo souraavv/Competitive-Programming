913c2bc2b4fd418cd4c4c7dc7b74666c2553bdbc
Sourav Sharma <souravsharma2468@gmail.com>
2021-07-25 16:46:44 +0530

    update Trie Data Structure;

---
+++ Trie.cpp
@@ -0,0 +1,37 @@
+class TrieNode {
+    public: 
+    TrieNode* child[26];
+    bool isEnd;
+
+    TrieNode () {
+        this->isEnd = false;
+        for (int i = 0; i < 26; ++i) {
+            this->child[i] = NULL;
+        }
+    }
+    void markEnd() {
+        this->isEnd = true;
+    }
+};
+
+class Solution {
+    public:
+    int ans = 0;
+    TrieNode* root = new TrieNode();
+    
+    void insertRec(TrieNode* itr, string s, int i) {
+        if (i == s.length()) {
+            itr->markEnd();
+            return;
+        }
+        int idx = s[i] - 'a';
+        if (itr->child[idx] == NULL) {
+            itr->child[idx] = new TrieNode();
+        }
+        insertRec(itr->child[idx], s, i + 1);
+        
+    }
+    void insert(string s) {
+        insertRec(root, s, 0);
+    }
+};
