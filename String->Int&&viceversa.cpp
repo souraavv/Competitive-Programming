471f56e4a5decc83d074cf84cd58559a2671db18
Sourav Sharma <souravsharma2468@gmail.com>
2022-11-07 20:23:58 +0530

    Fix the bug of not handling negative numbers.

--- String->Int&&viceversa.cpp
+++ String->Int&&viceversa.cpp
@@ -1,7 +1,10 @@
 
 string toStr(int x)
-{
+{	
 	string s = "";
+	if (x < 0) {
+		s += "-";
+	}
 	if (x == 0) return "0";
 	while(x > 0)
 	{
@@ -13,9 +16,17 @@ string toStr(int x)
 
 
 int toNum(string s)
-{
+{	
+	
 	int x = 0;
-	for (int i = 0; i < (int)s.length(); i++)
-		x = 10 * x + (int)(s[i] - '0');
-	return x;
+	bool neg = false;
+	for (int i = 0; i < (int)s.length(); i++) {
+		if (i == 0 and s[i] == '-') {
+			neg = true;
+		}
+		else {
+			x = 10 * x + (int)(s[i] - '0');
+		}
+	}
+	return neg ? -x : x;
 }
