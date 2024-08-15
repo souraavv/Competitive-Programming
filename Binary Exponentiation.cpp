74d1ee7022abb11edbca17b36a9a8d3a57e2eb19
Sourav Sharma <souravsharma2468@gmail.com>
2024-08-15 11:28:07 +0530

    Update Binary Exponentiation.cpp
    code cleaning.

--- Binary Exponentiation.cpp
+++ Binary Exponentiation.cpp
@@ -5,52 +5,53 @@ using namespace std;
 #define int long long 
 const int mod = 1e9+7;
 
-int pow(int a,int b){
+int pow(int a, int b) {
     int ans = 1;
-    while(b>0){
-        if(b%2)
-            ans*=a;
-        a*=a;
-        b>>=1;
+    while (b > 0) {
+        if (b & 1) {
+            ans *= a;
+        }
+        a *= a;
+        b >>= 1;
     }
     return ans;
 }
 
-int add(int a,int b){
-    a+=b;
-    if(a>=mod)
-        return a-mod;
+int add(int a, int b){
+    a += b;
+    if(a >= mod)
+        return a - mod;
     return a;
 }
 
-int sub(int a,int b){
-    a-=b;
-    if(a<0)
-        return a+mod;
+int sub(int a, int b){
+    a -= b;
+    if(a < 0)
+        return a + mod;
     return a;
 }
 
-int multiply(int a,int b){
-    int k=(a*b)%m;
+int multiply(int a, int b){
+    int k = (a * b) % m;
     return k;   
 }
-// since a.b mod m =[ (a mod m)*(b mod m) ]mod m ; since both a and be are same 
-int power(int a,int b,int m){
-    int ans=1;
-    a%=m;
-    while(b>0){
-        if(b%2)
-            ans=(ans*a)%m;
-        a=(a*a)%m;
-        b/=2;
+
+int power(int a, int b, int m){
+    int ans = 1;
+    a %= m;
+    while(b > 0) {
+        if(b & 1)
+            ans = (ans * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
     }
     return ans;
 }
 
 
 int32_t main(){
-    int k = pow(2,20);
-    int p = power(2,20,mod);
-    cout<<p<<"\n";
+    int k = pow(2, 20);
+    int p = power(2, 20, mod);
+    cout << p << '\n';
     
 }
